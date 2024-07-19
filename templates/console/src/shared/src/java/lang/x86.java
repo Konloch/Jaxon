8be50f47353716446f0cb4fe7046d4d6249d6212
Konloch <konloch@gmail.com>
2024-07-18 19:46:40 -0600

    Started x86 Instruction Constants

---
+++ templates/console/src/shared/src/java/lang/x86.java
@@ -0,0 +1,31 @@
+package java.lang;
+
+/**
+ * @author Konloch
+ * @since 7/18/2024
+ */
+public class x86
+{
+	//Push instructions
+	public static final int PUSH_BYTE = 0x6A;
+	public static final int PUSH_EAX = 0x50;
+	
+	//Load Effective Address instructions
+	public static final int LEA_EAX = 0x8D;
+	
+	//Call instructions
+	public static final int CALL_NEAR = 0xFF;
+	public static final int CALL_FAR = 0xFF;
+	
+	//ModRM instructions
+	public static final int MODRM_EAX = 0x45;
+	
+	//Immediate instructions
+	public static final int IMMEDIATE_BYTE = 0x6A;
+	public static final int IMMEDIATE_WORD = 0x68;
+	public static final int IMMEDIATE_DWORD = 0xB8;
+	
+	//Other instructions
+	public static final int PUSH_EBP_MINUS_4 = 0x45 | (0xFC << 8);
+	public static final int PUSH_EBP_PLUS_8 = 0x45 | (0x08 << 8);
+}
--- templates/console/src/windows/src/kernel/windows/WinSystem.java
+++ templates/console/src/windows/src/kernel/windows/WinSystem.java
@@ -1,17 +1,68 @@
 package kernel.windows;
 
+/**
+ * @author Konloch
+ * @author S. Frenz
+ */
 public class WinSystem extends System
 {
-	@Override
-	public void doPrintChar(int c)
+	public boolean isDirectory(String path)
 	{
-		MAGIC.inline(0x6A, 0x00);                            //push byte 0 (no overlap)
-		MAGIC.inline(0x8D, 0x45, 0xFC);                      //lea eax,[ebp-4] (address of result)
-		MAGIC.inline(0x50);                                  //push eax
-		MAGIC.inline(0x6A, 0x01);                            //push byte 1 (single character)
-		MAGIC.inline(0x8D, 0x45, 0x08);                      //lea eax,[ebp+8] (address of string)
-		MAGIC.inline(0x50);                                  //push eax
-		MAGIC.inline(0xFF, 0x35); MAGIC.inline32(rte.DynamicRuntime._hndStdOut); //push handle
-		MAGIC.inline(0xFF, 0x15); MAGIC.inline32(rte.DynamicRuntime._Kernel_WriteFile); //call
+		return false;
+	}
+	
+	public boolean doesExist(String path)
+	{
+		return false;
+	}
+	
+	public boolean delete(String path)
+	{
+		return false;
+	}
+	
+	public boolean createDirectory(String path)
+	{
+		return false;
+	}
+	
+	public boolean rename(String oldPath, String newPath)
+	{
+		return false;
+	}
+	
+	public long getSize(String path)
+	{
+		return 0;
+	}
+	
+	public String[] listDirectory(String path)
+	{
+		return null;
+	}
+	
+	public byte[] read(String path)
+	{
+		return new byte[0];
+	}
+	
+	public void write(String path, int offset, byte[] bytes, int length, boolean append)
+	{
+	
+	}
+	
+	@Override
+	public void printCharacter(int c)
+	{
+		MAGIC.inline(x86.PUSH_BYTE, 0x00);                      //push byte 0 (no overlap)
+		MAGIC.inline(x86.LEA_EAX, x86.MODRM_EAX, 0xFC);         //lea eax,[ebp-4] (address of result)
+		MAGIC.inline(x86.PUSH_EAX);                             //push eax
+		MAGIC.inline(x86.PUSH_BYTE, 0x01);                      //push byte 1 (single character)
+		MAGIC.inline(x86.LEA_EAX, x86.MODRM_EAX, 0x08);         //lea eax,[ebp+8] (address of string)
+		MAGIC.inline(x86.PUSH_EAX);                             //push eax
+		MAGIC.inline(x86.CALL_NEAR, 0x35);
+		MAGIC.inline32(rte.DynamicRuntime._hndStdOut);          //push handle
+		MAGIC.inline(x86.CALL_NEAR, 0x15);
+		MAGIC.inline32(rte.DynamicRuntime._Kernel_WriteFile);   //call
 	}
 }
