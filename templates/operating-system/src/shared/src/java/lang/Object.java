dbc4b6d9c1871d34d28d336b0f42b73c3c1765ea
Konloch <konloch@gmail.com>
2024-07-19 21:36:50 -0600

    Fix Operating-System Codebase

--- templates/operating-system/src/shared/src/java/lang/Object.java
+++ templates/operating-system/src/shared/src/java/lang/Object.java
@@ -7,5 +7,8 @@ import rte.SClassDesc;
  */
 public class Object
 {
-	public SClassDesc _r_type; //fixed by compiler: reloc-position 0
+	//all fields are set in DynamicRuntime.newInstance
+	public final int _r_relocEntries=0, _r_scalarSize=0; //fixed by compiler: scalar-position 0 / 1
+	public final SClassDesc _r_type=null; //fixed by compiler: reloc-position 0
+	public final Object _r_next=null; //fixed by compiler: reloc-position 1
 }
--- templates/operating-system/src/shared/src/rte/SArray.java
+++ templates/operating-system/src/shared/src/rte/SArray.java
@@ -5,6 +5,8 @@ package rte;
  */
 public class SArray
 {
-	public int length, _r_dim, _r_stdType;
-	public Object _r_unitType;
+	//all fields are set in DynamicRuntime.newArray
+	public final int length=0;
+	protected final int _r_dim=0, _r_stdType=0;
+	protected final SClassDesc _r_unitType=null; //PicOS will not support interface arrays
 }
