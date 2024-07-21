954c51862642e770a13f52525962384b8aaf5ed1
Konloch <konloch@gmail.com>
2024-07-21 10:06:25 -0600

    Added Math Max

--- templates/application-console/src/shared/src/java/lang/Math.java
+++ templates/application-console/src/shared/src/java/lang/Math.java
@@ -7,6 +7,41 @@ public class Math
 {
 	public final static double PI = 3.141592653589793238;
 	
+	public static byte max(byte a, byte b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
+	public static short max(short a, short b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
+	public static char max(char a, char b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
+	public static int max(int a, int b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
+	public static long max(long a, long b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
+	public static double max(double a, double b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
+	public static float max(float a, float b)
+	{
+		return (a >= b) ? a : b;
+	}
+	
 	public static double tan(double nr)
 	{
 		MAGIC.inline(0xDD, 0x45);
