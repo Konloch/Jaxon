515bec4eb6cfe60d08485948e50fcddb781a8cc6
Konloch <konloch@gmail.com>
2024-07-21 10:58:15 -0600

    Hello World Example

--- templates/application-console/src/shared/src/app/TestSystemAPI.java
+++ templates/application-console/src/shared/src/app/TestSystemAPI.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.io.ByteArrayOutputStream;
+
 /**
  * @author Konloch
  * @since 7/21/2024
@@ -8,6 +10,7 @@ public class TestSystemAPI
 {
 	public static void testFileSystem()
 	{
-		//System._system.write("test.txt", 0, );
+		ByteArrayOutputStream output = new ByteArrayOutputStream("Hello World\n".toByteArray());
+		System._system.write("test.txt", 0, output, true);
 	}
 }
