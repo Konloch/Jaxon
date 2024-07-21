9a9fb780cf97776dd2f1ecd1dad7dad9070e3f1f
Konloch <konloch@gmail.com>
2024-07-21 07:16:01 -0600

    Expanded Arrays Cloning API

--- templates/console/src/shared/src/java/lang/Arrays.java
+++ templates/console/src/shared/src/java/lang/Arrays.java
@@ -6,6 +6,48 @@ package java.lang;
  */
 public class Arrays
 {
+	public static boolean[] clone(boolean[] array)
+	{
+		return clone(array.length, array);
+	}
+	
+	public static boolean[] clone(int newCapacity, boolean[] array)
+	{
+		boolean[] newValue = new boolean[newCapacity];
+		for (int i = 0; i < array.length; i++)
+			newValue[i] = array[i];
+		
+		return newValue;
+	}
+	
+	public static byte[] clone(byte[] array)
+	{
+		return clone(array.length, array);
+	}
+	
+	public static byte[] clone(int newCapacity, byte[] array)
+	{
+		byte[] newValue = new byte[newCapacity];
+		for (int i = 0; i < array.length; i++)
+			newValue[i] = array[i];
+		
+		return newValue;
+	}
+	
+	public static short[] clone(short[] array)
+	{
+		return clone(array.length, array);
+	}
+	
+	public static short[] clone(int newCapacity, short[] array)
+	{
+		short[] newValue = new short[newCapacity];
+		for (int i = 0; i < array.length; i++)
+			newValue[i] = array[i];
+		
+		return newValue;
+	}
+	
 	public static char[] clone(char[] array)
 	{
 		return clone(array.length, array);
@@ -14,6 +56,34 @@ public class Arrays
 	public static char[] clone(int newCapacity, char[] array)
 	{
 		char[] newValue = new char[newCapacity];
+		for (int i = 0; i < array.length; i++)
+			newValue[i] = array[i];
+		
+		return newValue;
+	}
+	
+	public static int[] clone(int[] array)
+	{
+		return clone(array.length, array);
+	}
+	
+	public static int[] clone(int newCapacity, int[] array)
+	{
+		int[] newValue = new int[newCapacity];
+		for (int i = 0; i < array.length; i++)
+			newValue[i] = array[i];
+		
+		return newValue;
+	}
+	
+	public static long[] clone(long[] array)
+	{
+		return clone(array.length, array);
+	}
+	
+	public static long[] clone(int newCapacity, long[] array)
+	{
+		long[] newValue = new long[newCapacity];
 		for (int i = 0; i < array.length; i++)
 			newValue[i] = array[i];
 		
