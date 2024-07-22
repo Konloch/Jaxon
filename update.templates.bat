9e9236f6fff2913d043a4aaa6c07a7bb9722916d
Konloch <konloch@gmail.com>
2024-07-21 22:49:35 -0600

    Updated Templates Script

--- update.templates.bat
+++ update.templates.bat
@@ -1,2 +1,3 @@
 cd src\jaxon\src\main\resources\templates
 jaxon zip ../../../../../../templates/application-barebones barebones ../../../../../../templates/application-console console ../../../../../../templates/application-graphical graphical ../../../../../../templates/atmega atmega ../../../../../../templates/operating-system operating-system
+cd ../../../../../../
